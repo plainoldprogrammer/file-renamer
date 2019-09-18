013fea496e1069f5ae8ca1612fa78d46860058cf
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-09-18 13:49:04 -0500

    changing the ui name to be specific

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,56 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonSelectFolder = new System.Windows.Forms.Button();
+            this.labelFolderPath = new System.Windows.Forms.Label();
+            this.textBoxFolderPath = new System.Windows.Forms.TextBox();
+            this.buttonRename = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonSelectFolder
             // 
-            this.button1.Location = new System.Drawing.Point(376, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(28, 20);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "...";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.buttonSelectFolder.Location = new System.Drawing.Point(376, 44);
+            this.buttonSelectFolder.Name = "buttonSelectFolder";
+            this.buttonSelectFolder.Size = new System.Drawing.Size(28, 20);
+            this.buttonSelectFolder.TabIndex = 0;
+            this.buttonSelectFolder.Text = "...";
+            this.buttonSelectFolder.UseVisualStyleBackColor = true;
+            this.buttonSelectFolder.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // label1
+            // labelFolderPath
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Folder:";
+            this.labelFolderPath.AutoSize = true;
+            this.labelFolderPath.Location = new System.Drawing.Point(12, 27);
+            this.labelFolderPath.Name = "labelFolderPath";
+            this.labelFolderPath.Size = new System.Drawing.Size(39, 13);
+            this.labelFolderPath.TabIndex = 1;
+            this.labelFolderPath.Text = "Folder:";
             // 
-            // textBox1
+            // textBoxFolderPath
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 44);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(358, 20);
-            this.textBox1.TabIndex = 2;
+            this.textBoxFolderPath.Location = new System.Drawing.Point(12, 44);
+            this.textBoxFolderPath.Name = "textBoxFolderPath";
+            this.textBoxFolderPath.Size = new System.Drawing.Size(358, 20);
+            this.textBoxFolderPath.TabIndex = 2;
             // 
-            // button2
+            // buttonRename
             // 
-            this.button2.Location = new System.Drawing.Point(329, 87);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 24);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Rename Files";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonRename.Location = new System.Drawing.Point(329, 87);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(75, 24);
+            this.buttonRename.TabIndex = 3;
+            this.buttonRename.Text = "Rename Files";
+            this.buttonRename.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(428, 130);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.textBoxFolderPath);
+            this.Controls.Add(this.labelFolderPath);
+            this.Controls.Add(this.buttonSelectFolder);
             this.Name = "MainForm";
             this.Text = "File Renamer";
             this.ResumeLayout(false);
@@ -86,10 +86,10 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonSelectFolder;
+        private System.Windows.Forms.Label labelFolderPath;
+        private System.Windows.Forms.TextBox textBoxFolderPath;
+        private System.Windows.Forms.Button buttonRename;
     }
 }
 
