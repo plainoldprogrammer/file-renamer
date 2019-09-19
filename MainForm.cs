1f294a94e431ef0c113f2e8165193121df7cd1b2
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-09-18 21:21:51 -0500

    ordering the file paths in natural order

--- MainForm.cs
+++ MainForm.cs
@@ -7,9 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using System.Security;
+using System.Runtime.InteropServices;
+
 
 namespace file_renamer
 {
+    [SuppressUnmanagedCodeSecurity]
+    internal static class SafeNativeMethods
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        public static extern int StrCmpLogicalW(string psz1, string psz2);
+    }
+
+    public sealed class NaturalStringComparer : IComparer<string>
+    {
+        public int Compare(string a, string b)
+        {
+            return SafeNativeMethods.StrCmpLogicalW(a, b);
+        }
+    }
+
+    public sealed class NaturalFileInfoNameComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo a, FileInfo b)
+        {
+            return SafeNativeMethods.StrCmpLogicalW(a.Name, b.Name);
+        }
+    }
+
     public partial class MainForm : Form
     {
         public MainForm()
@@ -35,11 +61,15 @@ namespace file_renamer
         public static void ProcessDirectory(string path)
         {
             string[] fileEntries = Directory.GetFiles(path);
+            Array.Sort(fileEntries, new NaturalStringComparer());
+            
 
             foreach (string fileName in fileEntries)
             {
                 Console.WriteLine(fileName);
             }
+
+            Console.WriteLine(fileEntries.Length);
         }
     }
 }
